521787acfef18118c2a1bf8a1daf973cd443a0d7
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-05-08 17:18:38 +0300

    Update sort.js

--- sort.js
+++ sort.js
@@ -2,7 +2,3 @@ const sorting_stuff = ['1','2','3']
 sorting_stuff.soft();
 const sorting_stuff = ['1','2','3']
 sorting_stuff.soft();
-const sorting_stuff = ['1','2','3']
-sorting_stuff.soft();
-const sorting_stuff = ['1','2','3']
-sorting_stuff.soft();
